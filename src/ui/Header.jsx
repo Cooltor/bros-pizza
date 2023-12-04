c8dedfeeb98f0c1dce92ab25da64c7bbf6a75da6
Cooltor <romaindbus86@gmail.com>
2023-12-04 18:48:37 +0100

    app layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Yummies Bros Pizzas Co.</Link>
+
+      <p>Romain</p>
+    </header>
+  );
+}
+
+export default Header;
