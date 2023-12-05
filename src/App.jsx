1b79aa4bfde86b34a33590aa51ba2052fc15314d
Cooltor <romaindbus86@gmail.com>
2023-12-05 14:32:14 +0100

    tailwind css setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import Home from "./ui/Home";
 import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
 import Order, { loader as orderLoader } from "./features/order/Order";
+
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
@@ -30,6 +33,8 @@ const router = createBrowserRouter([
       {
         path: "/order/new",
         element: <CreateOrder />,
+        action: createOrderAction,
+        errorElement: <Error />,
       },
       {
         path: "/order/:orderId",
