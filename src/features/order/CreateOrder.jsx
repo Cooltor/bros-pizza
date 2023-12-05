edbd532793b31e6ab543cb6b96b76fb2120c741c
Cooltor <romaindbus86@gmail.com>
2023-12-05 18:42:45 +0100

    tailwind styling app

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -6,7 +6,7 @@ import { createOrder } from "../../services/apiRestaurant";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
@@ -63,7 +63,14 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input
+              type="text"
+              name="address"
+              required
+              className="w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all
+               duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400
+               focus:ring-opacity-50 md:px-6 md:py-3 "
+            />
           </div>
         </div>
 
@@ -74,13 +81,21 @@ function CreateOrder() {
             id="priority"
             value={withPriority}
             onChange={(e) => setWithPriority(e.target.checked)}
+            className="h-6 w-6 rounded border-gray-300 accent-yellow-400 focus:ring-yellow-400 focus:ring-offset-2"
           />
           <label htmlFor="priority">Want to yo give your order priority?</label>
         </div>
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button
+            disabled={isSubmitting}
+            className="inline-block rounded-full bg-yellow-500 px-4 py-3 font-semibold 
+            uppercase tracking-wide text-stone-800 transition-colors duration-300
+             hover:bg-yellow-400 focus:bg-yellow-300 focus:outline-none focus:ring
+              focus:ring-yellow-400 focus:ring-offset-2 active:bg-slate-400 
+              disabled:cursor-not-allowed"
+          >
             {isSubmitting ? "Placing order..." : "Order now"}
           </button>
         </div>
